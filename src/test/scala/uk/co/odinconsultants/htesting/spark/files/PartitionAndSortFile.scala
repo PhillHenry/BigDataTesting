861dde47ab8efd673a1a1b1bb3869e632b925eb7
PhillHenry <phillhenry@gmail.com>
2018-12-06 10:44:42 +0000

    Needs tidying but demonstrates that a sort before writing sorts the parquet file. Still not seeing the stats tho.

--- src/test/scala/uk/co/odinconsultants/htesting/spark/files/PartitionAndSortFile.scala
+++ src/test/scala/uk/co/odinconsultants/htesting/spark/files/PartitionAndSortFile.scala
@@ -4,6 +4,7 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.parquet.format.converter.ParquetMetadataConverter
 import org.apache.parquet.hadoop.ParquetFileReader
+import org.apache.parquet.hadoop.metadata.BlockMetaData
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
@@ -16,10 +17,13 @@ class PartitionAndSortFile extends WordSpec with Matchers {
   "partition and sorting" should {
     "be seen in parquet files" in {
       import session.implicits._
-      val data          = (1 to 10000).map(i => (i, ('A' + i % 25) + 10240, i % 2))
+      val nSlots        = 200
+      val data          = (1 to 10000).map(i => (i, (('A' + i % 25).toString * 1024).toString, i % 2, i % nSlots))
+      println("data head: " + data.head)
       val partitionkey  = "partitionkey"
       val text          = "text"
-      val df            = data.toDF("id", text, partitionkey)
+      val intKey        = "intkey"
+      val df            = data.toDF("id", text, partitionkey, intKey)
 
       val filename = hdfsUri + System.currentTimeMillis()
 
@@ -33,20 +37,48 @@ class PartitionAndSortFile extends WordSpec with Matchers {
 //      df.write.partitionBy(partitionkey).sortBy(text).bucketBy(10, text).parquet(filename)
 
 
-      df.sort(text).write.partitionBy(partitionkey).parquet(filename)
+      df.sort(intKey).write.partitionBy(partitionkey).parquet(filename)
 
-
-      val files = list(filename)
+      val files = list(filename).filter(_.toString.endsWith(".parquet"))
       println("Files:\n" + files.mkString("\n"))
-      files.filter(_.toString.endsWith(".parquet")).foreach { file =>
-        val reader = ParquetFileReader.readFooter(new Configuration(), file, ParquetMetadataConverter.NO_FILTER)
+
+      val fromHdfs = session.read.parquet(filename)
+
+      println("Schema: ")
+      fromHdfs.printSchema() // Note the schema changes. The partition key goes to the end of the list of columns
+
+
+      files.foreach { file =>
+        val conf = new Configuration()
+        conf.setBoolean("parquet.strings.signed-min-max.enabled", true)
+        val reader = ParquetFileReader.readFooter(conf, file, ParquetMetadataConverter.NO_FILTER)
         println("Reader = " + reader)
         import scala.collection.JavaConversions._
         import scala.collection.JavaConverters._
         println("blocks = " + reader.getBlocks)
         println("blocks = " + reader.getBlocks.toList.mkString("\n"))
         println("stats:")
-        reader.getBlocks.toList.foreach(x => println(x.getColumns.toList.foreach(_.getStatistics)))
+        reader.getBlocks.toList.foreach(blockMetaData => println(blockMetaData.getColumns.toList.foreach(_.getStatistics)))
+        println("Columns")
+        reader.getBlocks.toList.foreach(blockMetaData => println(blockMetaData.getColumns.toList.mkString("\n")))
+      }
+
+//      Thread.sleep(Long.MaxValue)
+
+
+      val intsPerPartition = fromHdfs.mapPartitions { xs =>
+        val ids = xs.map { _.getInt(2) }.toSet
+        Iterator(ids)
+      }
+
+      println(intKey + ": " + fromHdfs.map {_.getInt(2) }.distinct().collect().mkString(", "))
+
+      val inMem: Array[Set[Int]] = intsPerPartition.collect()
+      val nPartitions = fromHdfs.rdd.partitions.size
+      val expectedNumPerSlot = nSlots / nPartitions
+      println(s"number of partitions = $nPartitions, expected number per slot = $expectedNumPerSlot, sizes = ${inMem.map(_.size).mkString(", ")}")
+      withClue(s"ints in a given partition:\n${inMem.mkString("\n")}\nExpected number = $expectedNumPerSlot\nNum of partitions = $nPartitions") {
+        inMem.foreach { _.size shouldBe expectedNumPerSlot +- expectedNumPerSlot.toInt }
       }
     }
   }
