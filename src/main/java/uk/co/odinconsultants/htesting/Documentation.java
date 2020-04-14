ef86d1c82a632908ea6e47377a417367097f9b00
PhillHenry <phillhenry@gmail.com>
2020-04-14 11:27:19 +0100

    Can now be deployed to Sonatype.

---
+++ src/main/java/uk/co/odinconsultants/htesting/Documentation.java
@@ -0,0 +1,7 @@
+package uk.co.odinconsultants.htesting;
+
+/**
+* HACK! This is here so a javadoc JAR can be generated that is needed by Sonatype for deployment.
+*/
+public class Documentation {
+}
